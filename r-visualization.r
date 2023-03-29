4c4d97fd52b1fdca9113a9d714171cfc35fdbd66
Michal Tesnar <michal.tesnar007@gmail.com>
2023-03-29 16:56:43 +0200

    fixed it

--- r-visualization.r
+++ r-visualization.r
@@ -5,8 +5,4 @@ meanData = aggregate(data$ticks, list(data$food_amount, data$blob_count), mean)
 names(meanData)[3] <- "Ticks"
 ggplot(meanData, aes(x=Group.1, y=Group.2, fill = Ticks)) + 
   geom_tile() + xlab("Amount of food") + ylab("Number of blobs") +
-<<<<<<< Updated upstream
   scale_fill_viridis_c()
-=======
-  scale_fill_viridis_c()
->>>>>>> Stashed changes
